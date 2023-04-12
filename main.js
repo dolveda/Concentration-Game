f0b86de7dffb0532535e12b0f1d0ea729f3924b2
dolveda <dolveda0@gmail.com>
2023-04-11 20:19:47 -0700

    MVP: Win and Loss Logic

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // 2) The player will start the game by clicking on a card, triggering an event listener to call a function which will turn the card over, displaying the value of the card.
 // 3) The player will continue their turn by clicking and turning over a second card.
 // 4) If the next card selected matches the previously turned card, the player's turn will continue and begin the pairing function again.
-// 4B) If the next card selected does not match the previously turned card, both cards will revert to the "back" position and the player's current turn will end. The Turn indicator will increase by 1, up to 3 turns total.
+// 4B) If the next card selected does not match the previously turned card, both cards will revert to the "back" position and the player's current turn will end. The Turn indicator will decrease by 1, up to 3 turns total.
 // 5) If the player is able to match all 6 pairs of cards before all 3 turns have been exhausted, the player wins, the Turn indicator will change to display the message "You Win!!!", and the game will end.
 // 5B) If the player ends all 3 turns without matching all 6 pairs of cards, the player loses, the Turn indicator will change to the message "Try Again", and the game will end.
 // 6) Once the game ends with either result, the "Play Again" button will no longer be hidden and, once clicked, will re-initalize the game.
@@ -26,44 +26,42 @@ const cardChoices = [
         card: 'Coatl(Snake)',
         img: 'Images/coatl.png'
     },
-    // {
-    //     card: 'Cozcacuauhtli(Vulture)',
-    //     img: 'Images/cozcacuauhtli.png'
-    // },
-    // {
-    //     card: 'Cozcacuauhtli(Vulture)',
-    //     img: 'Images/cozcacuauhtli.png'
-    // },
-    // {
-    //     card: 'Cuauhtli(Eagle)',
-    //     img: 'Images/cuauhtli.png'
-    // },
-    // {
-    //     card: 'Cuauhtli(Eagle)',
-    //     img: 'Images/cuauhtli.png'
-    // },
-    // {
-    //     card: 'Itzcuintli(Dog)',
-    //     img: 'Images/itzcuintli.png'
-    // },
-    // {
-    //     card: 'Itzcuintli(Dog)',
-    //     img: 'Images/itzcuintli.png'
-    // },
-    // {
-    //     card: 'Mazatl(Deer)',
-    //     img: 'Images/mazatl.png'
-    // },
-    // {
-    //     card: 'Mazatl(Deer)',
-    //     img: 'Images/mazatl.png'
-    // }
+    {
+        card: 'Cozcacuauhtli(Vulture)',
+        img: 'Images/cozcacuauhtli.png'
+    },
+    {
+        card: 'Cozcacuauhtli(Vulture)',
+        img: 'Images/cozcacuauhtli.png'
+    },
+    {
+        card: 'Cuauhtli(Eagle)',
+        img: 'Images/cuauhtli.png'
+    },
+    {
+        card: 'Cuauhtli(Eagle)',
+        img: 'Images/cuauhtli.png'
+    },
+    {
+        card: 'Itzcuintli(Dog)',
+        img: 'Images/itzcuintli.png'
+    },
+    {
+        card: 'Itzcuintli(Dog)',
+        img: 'Images/itzcuintli.png'
+    },
+    {
+        card: 'Mazatl(Deer)',
+        img: 'Images/mazatl.png'
+    },
+    {
+        card: 'Mazatl(Deer)',
+        img: 'Images/mazatl.png'
+    }
 ];
 
-const matchImg  = 'Images/turquoise.png';
 
 /*----- cached elements  -----*/
-const gameBoard = document.querySelectorAll('.cards');
 
 const matchIndicator = document.getElementById('match');
 
@@ -71,7 +69,7 @@ const playAgainBtn = document.querySelector('button');
 
 const gameBoardArr = [...document.querySelectorAll('.cards')];
 
-const turnsLeft = document.getElementById('turnNumber');
+const turnNumber = document.getElementById('turnNumber');
 
 const cards = document.querySelectorAll('.cards img');
 
@@ -83,9 +81,14 @@ let userPickIds = [];
 
 let matchedCards = [];
 
+let turnsLeft = 4;
+
 /*----- event listeners -----*/
 gameBoardArr.forEach((card, idx) => {
     card.addEventListener('click', function() {
+        if(turnsLeft === 0) {
+            return;
+        }
         let chosenCard = cardChoices[idx];
         card.firstChild.setAttribute('src', chosenCard.img);
         card.firstChild.setAttribute('data-id', idx);
@@ -101,24 +104,42 @@ gameBoardArr.forEach((card, idx) => {
                 matchIndicator.innerHTML = `Cualli! You Matched ${cardChoices[cardID].card}!`;
                 cards[userPickIds[0]].removeEventListener('click', chosenCard);
                 cards[userPickIds[1]].removeEventListener('click', chosenCard);
-                matchedCards.push(userPicks);               
+                matchedCards.push(userPicks); 
+                console.log(matchedCards)              
             } else {
                 cards[userPickIds[0]].setAttribute('src', 'Images/tlahtolli.png');
                 cards[userPickIds[1]].setAttribute('src', 'Images/tlahtolli.png');
-                matchIndicator.innerHTML  = 'Zampa! Try Again!'
+                matchIndicator.innerHTML  = 'Zampa! Try Again!';
+                function decreaseTurns()  {
+                    if(turnsLeft >= 1) {
+                        turnsLeft--;
+                        turnNumber.innerHTML = turnsLeft;
+                    }
+                }
+                decreaseTurns();                
             }
             if(userPickIds[0] === userPickIds[1]) {
-                matchIndicator.innerHTML = "Axcualli! You can't pick the same card!"
+                matchIndicator.innerHTML = "Axcualli! You can't pick the same card!";
+                matchedCards.pop(userPicks);
+                cards[userPickIds[0]].setAttribute('src', 'Images/tlahtolli.png');  
             }
             userPicks = [];
             userPickIds =[];
+        if(turnsLeft < 1) {
+            playAgainBtn.style.visibility = 'visible';
+            matchIndicator.innerHTML = 'Out of Turns! Amapatoa Occeppa! (Play Again!)';
+        }
+
+        if(matchedCards.length === (cardChoices.length/2)) {
+            matchIndicator.innerHTML = 'Cualtitoc! Tatlani!(Great job! You Win!)';
+            playAgainBtn.style.visibility = 'visible';
+        }
         };
     })
 })
 
-
+playAgainBtn.addEventListener('click', playAgain);
 /*----- functions -----*/
-
 
 function randomizeCards(arr) {
     for (let i = arr.length - 1; i > 0;  i --) {
@@ -128,12 +149,6 @@ function randomizeCards(arr) {
 }
 randomizeCards(cardChoices);
 
-
-
-
-
-
-
-
-
-
+function playAgain() {
+    location.reload();
+}
